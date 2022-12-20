5f739ac44a01575acf946c8f7aee95882b17ffbc
ShuYangConlany <113554935+ShuYangConlany@users.noreply.github.com>
2022-12-20 10:33:02 -0500

    Add files via upload

---
+++ project 2/sentiment.py
@@ -0,0 +1,20 @@
+
+from google.cloud import language_v1
+import six
+
+def sample_analyze_sentiment(content):
+
+    client = language_v1.LanguageServiceClient()
+
+    # content = 'Your text to analyze, e.g. Hello, world!'
+
+    if isinstance(content, six.binary_type):
+        content = content.decode("utf-8")
+
+    type_ = language_v1.Document.Type.PLAIN_TEXT
+    document = {"type_": type_, "content": content}
+
+    response = client.analyze_sentiment(request={"document": document})
+    sentiment = response.document_sentiment
+    print("Score: {}".format(sentiment.score))
+    print("Magnitude: {}".format(sentiment.magnitude))
